c17fe3cf41e6a8b1bd31c3bf184c9a9fbbadc215
hector <hectorrrivera1010@gmail.com>
2023-11-09 23:07:19 -0600

    [const acceleration] add final note

--- 02-consaccel.py
+++ 02-consaccel.py
@@ -19,18 +19,29 @@ x = 1.33
 v = 0.0
 a = 0.0577
 ts = 1.4
-dt = 0.5
+dt = 0.3
 t = 0
 
 f1 = gcurve(color = color.red)
+
+"""
+Por que hace un ciclo mientras t (0) sea menor a ts (1.4)?
+es para que no haga una grafica parabolica y haga una mas adecuada a la aceleracion de 
+el mobil.
+"""
 
 while t < ts:
     t = t + dt
     f1.plot(t, x)
     
 while t < 4.4:
-    v = v + a * dt
-    x = x + v * dt
-    t = t + dt
+    v = v + a * dt # aqui se actializa la velocidad agarrando la velocidad actual y la suma por la multiplicacion de la aceleracion y el tiempo
+    x = x + v * dt # Aqui actualiza la distancia con la original y la suma a la velocidad actual multiplicada por el tiempo
+    t = t + dt # Aqui actualiza el tiempo sumandolo con el intervalo dt que teniamos
     
     f1.plot(t, x)
+    
+    
+'''
+NOTA: entre mas chiquito sea dt, mas definida sera la grafica
+'''
