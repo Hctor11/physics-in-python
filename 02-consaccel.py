fb4906c2030b7cbe2d8256091b2396c2f204c542
hector <hectorrrivera1010@gmail.com>
2023-11-09 22:14:15 -0600

    [const acceleration] first description

---
+++ 02-consaccel.py
@@ -0,0 +1,12 @@
+from vpython import *
+
+# CONSTANT ACCELERATION / ACCELERACION CONSTANTE
+
+"""
+Dado a que tenemos un valor independiente como el tiempo y un valor que depende
+de este como lo es la velocidad, podemos graficar ambos valores, siendo la variable 
+independiente el tiempo y la variable dependiente la velocidad.
+
+Al graficar los valores de un mobil que se mueve a velocidad constante nos dara una
+grafica perteneciente a una ecuacion lineal (una linea inclinada).
+"""
