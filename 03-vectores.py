2c34e082788cc0a831f41d2a171c621c7cd06b96
hector <hectorrrivera1010@gmail.com>
2023-11-12 00:08:17 -0600

    add vectors

---
+++ 03-vectores.py
@@ -0,0 +1,12 @@
+from vpython import *
+
+# VECTORES
+
+"""
+Para entender a los vectores, primero se tiene que entender que es un escalar,
+un escalar es una variable que depende solo de una cantidad para ser medida, por ejemplo la
+temperatura, la masa o la altura.
+
+un vector es una variable que no solo depende de una sola cantidad sino que puede ser de dos
+o mas.
+"""
