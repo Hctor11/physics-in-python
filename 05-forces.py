0357eae0e02ac9dbd9ef0e6ee32c711bc6d1253a
hector <hectorrrivera1010@gmail.com>
2023-11-13 20:00:16 -0600

    [add] forces

---
+++ 05-forces.py
@@ -0,0 +1,15 @@
+from vpython import *
+
+# FUERZAS
+
+"""
+QUE ES UNA FUERZA?
+Una fuerza es un vector que representa la interaccion entre dos objetos con masa. 
+
+La magnitud de una fuerza depende de la masa y de la distancia entre un objeto con
+el otro.
+
+
+
+
+"""
